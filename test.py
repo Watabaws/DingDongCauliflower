9ef7cc8c238b81f98c7afa9c921ce705cd2ea3f8
Adam Abbas <aabbas@stuy.edu>
2017-10-16 18:12:35 -0400

    Testing stuffs

--- test.py
+++ test.py
@@ -13,7 +13,9 @@ d.execute(init_table)
 iterThrough = d.execute("SELECT id FROM peeps;")
 
 for di in iterThrough:
-    command = "INSERT INTO grades_n_average VALUES (" + 
+	command = "SELECT mark FROM courses WITH id=" + str(di[0]) + ";"
+	print d.execute(command)
+    #command = "INSERT INTO grades_n_average VALUES (" + d.execute("SELECT name FROM peeps WITH id =" + di + ";") + ", " + di +
 
 db.commit() #Commit our changes! It's official!!!
 db.close() #Close the file connection
