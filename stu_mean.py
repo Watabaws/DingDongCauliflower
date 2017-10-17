dbe636a9204c0fec7b041b015d5e13a074e90a19
ykadir42 <ykadir42@gmail.com>
2017-10-17 18:42:22 -0400

    renamed table

--- stu_mean.py
+++ stu_mean.py
@@ -3,10 +3,10 @@ import sqlite3 #To write to the database
 pc="peeps_n_courses.db" #This is gonna be our file
 
 db = sqlite3.connect(pc) #Create/establish a connection with our file
-d = db.cursor() #Enable changes
-e = db.cursor() #Second cursor for grades
+d = db.cursor() #Checks database
+e = db.cursor() #Second cursor for editing database
 
-init_table = "CREATE TABLE grades_n_average (name TEXT, id NUMERIC PRIMARY KEY, average NUMERIC);"
+init_table = "CREATE TABLE peeps_avg (name TEXT, id NUMERIC PRIMARY KEY, average NUMERIC);"
 
 d.execute(init_table)
 
@@ -15,26 +15,27 @@ def get_ids():
 
 def compute_average(di):
 	command = "SELECT mark FROM courses WHERE id = " + str(di) + ";"
-	mus = 0.0	
-	ctr = 0.0	
+	mus = 0.0
+	ctr = 0.0
 	for grade in e.execute(command):
-		ctr += 1	
+		ctr += 1
 		mus += grade[0]
 	return mus / ctr
-	
 
-'''print iterThrough
-for di in iterThrough:
-    print "di:" + str(di[0])
-    command = "SELECT mark FROM courses WHERE id = " + str(di[0]) + ";"
-    for blah in e.execute(command):
-        print blah[0]
-'''
+
+
+# print iterThrough
+# for di in iterThrough:
+#     print "di:" + str(di[0])
+#     command = "SELECT mark FROM courses WHERE id = " + str(di[0]) + ";"
+#     for blah in e.execute(command):
+#         print blah[0]
+
 
 def insert_stud_record():
 	for di in get_ids():
 		name = e.execute("SELECT name FROM peeps WHERE id =" + str(di[0]) + ";")
-		command = "INSERT INTO grades_n_average VALUES ('" + str(name.fetchall()[0][0]) + "', " + str(di[0]) + ", " + str(compute_average(di[0])) + ");"
+		command = "INSERT INTO peeps_avg VALUES ('" + str(name.fetchall()[0][0]) + "', " + str(di[0]) + ", " + str(compute_average(di[0])) + ");"
 		e.execute(command)
 
 insert_stud_record()
