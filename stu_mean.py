2caba383f2ba9a8434eee1bcf2f5115344c03763
Adam Abbas <aabbas@stuy.edu>
2017-10-17 20:00:32 -0400

    Commented and Shnazzy

--- stu_mean.py
+++ stu_mean.py
@@ -11,21 +11,21 @@ db = sqlite3.connect(pc) #Create/establish a connection with our file
 d = db.cursor() #Checks database
 e = db.cursor() #Second cursor for editing database
 
-init_table = "CREATE TABLE peeps_avg (name TEXT, id NUMERIC PRIMARY KEY, average NUMERIC);"
+init_table = "CREATE TABLE peeps_avg (name TEXT, id NUMERIC PRIMARY KEY, average NUMERIC);" #Create a table in our database, that'll store peeps and their averages!
 
-d.execute(init_table)
+d.execute(init_table) #Create that table
 
-def get_ids():
-	return d.execute("SELECT id FROM peeps;")
+def get_ids(): #Get a list of all the ids our peeps have
+	return d.execute("SELECT id FROM peeps;") #Select the ids from our table
 
-def compute_average(di):
-	command = "SELECT mark FROM courses WHERE id = " + str(di) + ";"
-	mus = 0.0
-	ctr = 0.0
-	for grade in e.execute(command):
-		ctr += 1
-		mus += grade[0]
-	return mus / ctr
+def compute_average(di): #Get the average for a given ID
+	command = "SELECT mark FROM courses WHERE id = " + str(di) + ";" #Get all of the grades for the inputted ID
+	mus = 0.0 #Set the total sum to 0
+	ctr = 0.0 #Set the counter to 0
+	for grade in e.execute(command): #loop through all of the grades
+		ctr += 1 #Add one to number of courses
+		mus += grade[0] #Sum up this grade
+	return mus / ctr #Return the average!!
 
 
 
@@ -37,33 +37,28 @@ def compute_average(di):
 #         print blah[0]
 
 
-def insert_stud_record():
-	for di in get_ids():
-		name = e.execute("SELECT name FROM peeps WHERE id =" + str(di[0]) + ";")
-		command = "INSERT INTO peeps_avg VALUES ('" + str(name.fetchall()[0][0]) + "', " + str(di[0]) + ", " + str(compute_average(di[0])) + ");"
-		e.execute(command)
+def insert_stud_record(): #code to insert the students, id and averages
+	for di in get_ids(): #Loop through all of our ids
+		name = e.execute("SELECT name FROM peeps WHERE id =" + str(di[0]) + ";") #Get the name of a student with our current id
+		command = "INSERT INTO peeps_avg VALUES ('" + str(name.fetchall()[0][0]) + "', " + str(di[0]) + ", " + str(compute_average(di[0])) + ");" #Add a record to our peeps_avg table with the student name, id and their computed average;
+		e.execute(command) #Execute that command yurr
 
-insert_stud_record()
+insert_stud_record() #Actually run the code to insert the records
 
-def print_averages():
-	for di in get_ids():
-		name = e.execute("SELECT name FROM peeps WHERE id =" + str(di[0]) + ";")
-		print "name: " + str(name.fetchall()[0][0]) + " | id: " + str(di[0]) + " | avg: " + str(compute_average(di[0]))
+def print_averages(): #Code to get just the averages
+	for di in get_ids(): #Loop through all of our IDs
+		name = e.execute("SELECT name FROM peeps WHERE id =" + str(di[0]) + ";") #get the name of our current ID
+		print "name: " + str(name.fetchall()[0][0]) + " | id: " + str(di[0]) + " | avg: " + str(compute_average(di[0])) #Print out name, id, and average
 
 print_averages()
 
-#TODO
-# Facilitate updating a student's average
-# Facilitate adding rows to the courses table
-# To test the last 2 features, you should add lines to the end of courses.csv to reflect a new term's worth of course enrollment for your peeps (students).
+def update_average(di): #Just in case we finally cop that grade change
+	command = "UPDATE peeps_avg SET avg = " + str(compute_average(di[0])) + " WHERE id = " + str(di[0]) + ";" #Updates the specified id's average with whatever is in the csv
+	e.execute(command) #Actually do it uk
 
-def update_average(di):
-	command = "UPDATE peeps_avg SET avg = " + str(compute_average(di[0])) + " WHERE id = " + str(di[0]) + ";"
-	e.execute(command)
-
-def add_course(di, code, mark):
-	command = "INSERT INTO courses VALUES ('" + di + "', " + code + ", " + mark + ");"
-	e.execute(command)
+def add_course(di, code, mark): #Add a single course
+	command = "INSERT INTO courses VALUES ('" + di + "', " + code + ", " + mark + ");" #Add a record into the courses table
+	e.execute(command) #actually do it!!
 
 db.commit() #Commit our changes! It's official!!!
 db.close() #Close the file connection
